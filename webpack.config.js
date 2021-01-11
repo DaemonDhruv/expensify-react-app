25a2a223aa9e362308adfabbc4fdfa20ab888db9
DaemonDhruv <dhruv.bindoria@gmail.com>
2021-01-11 20:54:52 +0530

    Configured for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,35 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'build.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true // this will tell the server that we have client side routing and server will always serve index.js
+// We can export a function which will return an object
+// We need to specify the build command of webpack in the package.json to call this function and pass the correct argument
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'build.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true // this will tell the server that we have client side routing and server will always serve index.js
+        }
     }
-};
+}
